16af999ef1c6ab9810d9f546567c648c68d695ab
Ren Kimura <renk@ricsec.co.jp>
2025-08-10 15:13:40 +0900

    5

--- vulnserver.c
+++ vulnserver.c
@@ -1,4 +1,4 @@
-// Version 4/30
+// Version 5/30
 // file: net_maze.c
 // Build: gcc -Wall -O2 -o net_maze net_maze.c
 #include <stdio.h>
@@ -40,7 +40,24 @@ ssize_t recv_line(int fd, char *buf, size_t maxlen) {
 }
 
 int main(void){
-    printf("Listening (not implemented) on %d\n", PORT);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) { perror("socket"); return 1; }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind"); close(server_fd); return 1;
+    }
+    if (listen(server_fd, 4) < 0) {
+        perror("listen"); close(server_fd); return 1;
+    }
+    printf("Listening on port %d...\n", PORT);
+
+    close(server_fd);
     return 0;
 }
 

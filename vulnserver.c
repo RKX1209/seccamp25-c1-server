611b77a0f9d1c098fad56219ca7895cc9dae29e7
Ren Kimura <renk@ricsec.co.jp>
2025-08-10 15:12:48 +0900

    3

--- vulnserver.c
+++ vulnserver.c
@@ -1,4 +1,4 @@
-// Version 2/30
+// Version 3/30
 // file: net_maze.c
 // Build: gcc -Wall -O2 -o net_maze net_maze.c
 #include <stdio.h>
@@ -11,6 +11,15 @@
 
 #define MAX_LEN 512
 #define PORT 4444
+
+void send_all(int fd, const char *buf, size_t len) {
+    (void)fd; (void)buf; (void)len;
+}
+
+ssize_t recv_line(int fd, char *buf, size_t maxlen) {
+    (void)fd; (void)buf; (void)maxlen;
+    return -1;
+}
 
 int main(void){
     printf("Listening (not implemented) on %d\n", PORT);

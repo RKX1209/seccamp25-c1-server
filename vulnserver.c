ec55cd005c88ce8c83b91ba9cda7cf20f685ce7f
Ren Kimura <renk@ricsec.co.jp>
2025-08-10 15:13:23 +0900

    4

--- vulnserver.c
+++ vulnserver.c
@@ -1,4 +1,4 @@
-// Version 3/30
+// Version 4/30
 // file: net_maze.c
 // Build: gcc -Wall -O2 -o net_maze net_maze.c
 #include <stdio.h>
@@ -13,12 +13,30 @@
 #define PORT 4444
 
 void send_all(int fd, const char *buf, size_t len) {
-    (void)fd; (void)buf; (void)len;
+    size_t sent = 0;
+    while (sent < len) {
+        ssize_t n = send(fd, buf + sent, len - sent, 0);
+        if (n <= 0) break;
+        sent += (size_t)n;
+    }
 }
 
 ssize_t recv_line(int fd, char *buf, size_t maxlen) {
-    (void)fd; (void)buf; (void)maxlen;
-    return -1;
+    size_t idx = 0;
+    while (idx < maxlen - 1) {
+        char c;
+        ssize_t n = recv(fd, &c, 1, 0);
+        if (n == 1) {
+            if (c == '\n') break;
+            buf[idx++] = c;
+        } else if (n == 0) {
+            break;
+        } else {
+            return -1;
+        }
+    }
+    buf[idx] = '\0';
+    return (ssize_t)idx;
 }
 
 int main(void){
